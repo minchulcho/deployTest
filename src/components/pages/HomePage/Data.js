9f4d96ecf8a357a6aa7e4c8a7de4af27eb196f56
minchulcho <minchulc@uci.edu>
2021-02-18 19:24:31 -0800

    3

--- src/components/pages/HomePage/Data.js
+++ src/components/pages/HomePage/Data.js
@@ -2,7 +2,7 @@ export const homeObjOne = {
   lightBg: false,
   lightText: true,
   lightTextDesc: true,
-  topLine: 'Exclusive Access',
+  topLine: 'Resume',
   headline: 'Unlimited Transactions with zero fees',
   description:
     'Get access to our exclusive diamond card that allows you to  send unlimited transactions without getting charged any fees',
